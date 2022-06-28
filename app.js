8c7ef362e63b751917d3e6b5ece0c88dde006ee1
André Luís Silva Medina <106404962+Andre-SMedina@users.noreply.github.com>
2022-06-28 16:48:10 -0300

    att

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const indexRoute = require("./routes/index");
 const connectDB = require("./database/connect");
 
 connectDB();
-app.use(express.static("public"));
+app.use(express.static("routes"));
 app.use(express.json());
 app.listen(port, () => console.log(`Rodando na porta ${port}.`));
 
