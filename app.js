d3d34d875240fcf7fb338a99b3a45f91454cbce3
André Luís Silva Medina <106404962+Andre-SMedina@users.noreply.github.com>
2022-06-28 21:44:32 -0300

    att

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ partialsDir: ["views/partials"] });
 const port = process.env.PORT || 8080;
 const app = express();
-const indexRoute = require("./routes/index");
+// const indexRoute = require("./routes/index");
 const connectDB = require("./database/connect");
 
 connectDB();
-app.use(express.static("routes"));
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.use(express.static("routes/public"));
 app.use(express.json());
-app.listen(port, () => console.log(`Rodando na porta ${port}.`));
 
-app.use("/", indexRoute);
+// app.use("/", indexRoute);
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(port, () => console.log(`Rodando na porta ${port}.`));
